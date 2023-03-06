37db6ca0d321820c7de1161d1899b56f65965195
bvasubabu1993 <bvasubabu1993@gmail.com>
2023-03-06 17:30:40 +0530

    this is latest update in address.java file

--- src/com/app/example/Address.java
+++ src/com/app/example/Address.java
@@ -4,6 +4,8 @@ public class Address {
 
 	public static void main(String[] args) {
 		System.out.println("hi this is address class");
+		System.out.println(" this is  for testing perpose");
+		System.out.println(" this is for new update");
 	}
 
 }
